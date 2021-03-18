3c859d8f37fc3336ae48b88fe54822b18cc75716
Kullanan Thanachotnan <akoranos@gmail.com>
2021-03-18 17:38:40 +0700

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,99 +1,62 @@
-import * as React from "react";
-import profile from "../images/photo.jpg";
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons'
+import { Avatar, Button, Col, Layout, Row, Space, Typography } from 'antd'
+import 'antd/dist/antd.css'
+import { Link } from 'gatsby'
+import * as React from 'react'
+import profile from '../images/photo.jpg'
 
 function IndexPage() {
+  const { Header, Footer, Content } = Layout
+  const { Text } = Typography
+
   return (
     <main>
       <link rel="icon" href="/favicon.ico" />
       <title>Kullanan Thanachotanan Site</title>
 
-      <div>
-        <div>
-          <div>
-            <div>
-              <div>
-                <p>
-                  <img src={profile} alt="profile" />
-                </p>
-                <p>
-                  <strong>Kullanan Thanachotanan</strong>
-                </p>
-                <p>Software Engineer</p>
-                <p>
-                  <a href="https://www.linkedin.com/in/kullanan-thanachotanan-7976b9206/">
-                    Linkedin
-                  </a>
+      <Layout className="h-screen">
+        <Header>
+          <Button type="text" className="text-white">
+            <Link to="/">Home</Link>
+          </Button>
+          <Button type="text" className="text-white">
+            <Link to="/about-me">About me</Link>
+          </Button>
+        </Header>
+        <Content className="p-8">
+          <Row className="w-full items-center justify-center">
+            <Space direction="vertical" className="flex items-center justify-center">
+              <Col>
+                <Avatar src={profile} alt="profile" size={200} />
+              </Col>
+              <Col>
+                <Text className="font-bold">Kullanan Thanachotanan</Text>
+              </Col>
+              <Text>Software Engineer</Text>
+            </Space>
+          </Row>
+        </Content>
+        <Footer className="w-full flex items-center justify-center">
+          <Row className="w-full">
+            <Col span={24}>
+              <Row className="flex items-end justify-end">
+                <Link
+                  href="https://www.linkedin.com/in/kullanan-thanachotanan-7976b9206/"
+                  className="mr-2"
+                >
+                  <LinkedinOutlined className="text-xl" />
+                </Link>
 
-                  <a href="https://github.com/kullthana">Github</a>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <div>
-                <h3>Education</h3>
-              </div>
-            </div>
-
-            <div>
-              <div>
-                <p>
-                  <strong>Software Engineering</strong>
-                </p>
-                <p>2010 - 2014</p>
-                <p>
-                  College of Art, Media and Technology in Chiang Mai University
-                </p>
-              </div>
-            </div>
-
-            <hr />
-
-            <div>
-              <div>
-                <h3>Experience</h3>
-              </div>
-            </div>
-
-            <div>
-              <div>
-                <p>
-                  <strong>Internship Developer</strong>
-                </p>
-                <p>August 2020 - March 2021</p>
-                <p>Artisan Digital Asia</p>
-              </div>
-            </div>
-
-            <hr />
-
-            <div>
-              <div>
-                <h3>Skills</h3>
-              </div>
-            </div>
-
-            <div>
-              <div>
-                <span>TypeScript</span>
-                <span>Graphql</span>
-                <span>React.js</span>
-                <span>NodeJs</span>
-                <span>Docker</span>
-                <span>Git</span>
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>PostgreSQL</span>
-                <span>jQuery</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                <Link href="https://github.com/kullthana">
+                  <GithubOutlined className="text-xl" />
+                </Link>
+              </Row>
+            </Col>
+          </Row>
+        </Footer>
+      </Layout>
     </main>
-  );
+  )
 }
 
-export default IndexPage;
+export default IndexPage
